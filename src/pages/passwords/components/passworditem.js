1f8226ceee82535972c7ccb1f457847ecc215378
davidrockkj <davidrockkj@gmail.com>
2023-11-22 16:55:28 -0300

    Criado o componente passworditem que irá conter a senha e oferecer a opção de deletar

---
+++ src/pages/passwords/components/passworditem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+
+export function passwordItem({ data, removePassword }) {
+  return (
+    <Pressable onLongPress={removePassword}>
+      <Text>{data}</Text>
+    </Pressable>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#0e0e0e',
+    padding: 14,
+    width: '100%',
+    marginBottom: 14,
+  }
+})
