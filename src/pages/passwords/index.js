0b7b94e906e88a09669114ef4df07481ecbeb173
davidrockkj <davidrockkj@gmail.com>
2023-11-22 15:47:24 -0300

    Estilizando tela 'passwords'

--- src/pages/passwords/index.js
+++ src/pages/passwords/index.js
@@ -1,9 +1,28 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export function Passwords(){
   return (
-    <View>
-      <Text>Minhas senhas</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, }}> {/* Usando para o texto não ficar escondido */}
+      <View style={styles.header}>
+        <Text style={styles.title}>Minhas senhas</Text>
+      </View>
+    </SafeAreaView>
   )
 }
+
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#392de9',
+    paddingTop: 58,
+    paddingBottom: 14,
+    paddingLeft: 14,
+    paddingRight: 14
+  },
+  title: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold',
+  }
+})
