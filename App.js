74c225b5eb269b17c9dec2634219df1f619a47ce
David Rocha <davidrockkj@gmail.com>
2023-11-28 20:50:17 -0300

    Deixando a statusBar dark

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 /* Importando o NavigationContainer */
 import { NavigationContainer } from '@react-navigation/native'; 
+import { StatusBar } from 'expo-status-bar';
 
 /* Importando o componente das Rotas */
 import { Routes } from './src/routes'
@@ -9,6 +10,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor="#0000FF" barStyle="light-content" />
       <Routes />
       {/* Chamando o componente das rotas */}
     </NavigationContainer>
